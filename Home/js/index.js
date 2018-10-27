b19be2220ee7fe6a97ea6235bd04f1a8d83efb8d
shiyu <shiyu.now@gmail.com>
2018-10-27 20:53:32 +0800

    +loading

--- Home/js/index.js
+++ Home/js/index.js
@@ -1,13 +1,21 @@
 var URL = 'http://176.122.153.17:8080/appProject/';
 
 (function () {
-  init();
+  imgBackground = new Image(); //每次加载完都会绘制一次图片
+  imgBackground.src = 'img/bg.jpg';
+  imgBackground.onload = function () { //背景图片加载完场
+    init();
+  };
 })();
 
 function init() {
+  document.getElementById('showBox').style.display='block';
+  document.getElementById('loadingImgBox').style.display='none';
   let dom = document.getElementsByClassName('title');
   for (let i = 0; i < dom.length; i++) {
     let jumpName = dom[i].dataset.name;
-    dom[i].href = URL + jumpName;
+    dom[i].addEventListener('click',function () {
+      window.location.href=URL+jumpName;
+    });
   }
 }
